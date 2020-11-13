3c945f59b745d0163fa2b09edbfa269dea750a8f
chenshenchao <chenshenchao@outlook.com>
2020-11-14 00:12:03 +0800

    微调。

--- main/runtime/node.h
+++ main/runtime/node.h
@@ -5,21 +5,23 @@
  * 抽象语法树节点类型。
  * 
  */
-typedef enum node_type_e {
+typedef enum node_type_e
+{
     NT_NIL = 0,
-    NT_NUMBER,          // 数字
-    NT_ADDITION,        // 加法
-    NT_SUBTRACTION,     // 减法
-    NT_MULTIPLICATION,  // 乘法
-    NT_DIVISION,        // 除法
-    NT_END = -1         // 结尾
+    NT_NUMBER,         // 数字
+    NT_ADDITION,       // 加法
+    NT_SUBTRACTION,    // 减法
+    NT_MULTIPLICATION, // 乘法
+    NT_DIVISION,       // 除法
+    NT_END = -1        // 结尾
 } node_type_t;
 
 /**
  * 分叉节点。
  * 
  */
-struct fork_s {
+struct fork_s
+{
     struct node_s *left;
     struct node_s *right;
 } fork_t;
@@ -28,14 +30,14 @@ struct fork_s {
  * 构建抽象语法树的节点。
  * 
  */
-typedef struct node_s {
+typedef struct node_s
+{
     enum node_type_e type;
     union {
         double number;
         struct fork_s fork;
     } value;
 } node_t;
-
 
 node_t *new_fork(node_type_t type, node_t *left, node_t *right);
 node_t *new_number(double number);
